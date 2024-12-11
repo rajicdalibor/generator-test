d6767b10836c6387a0289e1e1208af0592cd1ade
rajicdalibor <rajicdalibor@yahoo.com>
2024-12-11 14:10:27 +0100

    test

--- frontend/lib/services/hello_world_service.dart
+++
@@ -1,7 +0,0 @@
-import 'package:frontend/services/firebase_functions_service.dart';
-
-class HelloWorldService {
-  static Future<String?> helloWorld() async {
-    return await FirebaseFunctionsService.helloWorld();
-  }
-}
