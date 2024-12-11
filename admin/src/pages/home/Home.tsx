64cecc7f8b0a5650be7a478fd8ea4e19ff7dd015
rajicdalibor <rajicdalibor@yahoo.com>
2024-12-11 13:59:43 +0100

    test

--- admin/src/pages/home/Home.tsx
+++ admin/src/pages/home/Home.tsx
@@ -4,7 +4,7 @@ import { FC } from 'react';
 export const HomePage: FC = () => {
   return (
     <Box display={'flex'} justifyContent={'center'} mt={5}>
-      <Typography variant="h4">Welcome to AIAB Admin Portal</Typography>
+      <Typography variant="h4">Welcome to AIAB Admin Portal1</Typography>
     </Box>
   );
 };
